b8ad39cf610c021ec2eb82bb8b42b835f747ac57
Naveen R <rk.naveenkumar@gmail.com>
2021-08-01 23:05:13 +0530

    First Commit adding 3 files

---
+++ ShellTest1.sh
@@ -0,0 +1,13 @@
+#!bin/sh
+clear
+echo "What is your name?"
+read name
+echo "How are you $name?"
+echo "Good to hear $name"
+echo "What is your age?"
+read age
+echo "Your Age: $age"
+echo "Nice to meet you $name"
+echo "By for now $name"
+#Testvar = "Bye"
+#echo $Testvar
---
+++ shtest.sh
@@ -0,0 +1,5 @@
+#!bin/sh
+clear
+echo "Hello World"
+echo "This is Naveen Rachakonda"
+ls -ltSa
